c0d3c964a6d32f7bcbdbbb7b50cd992165bedf41
Giancarlo <giancarlorondo@outlook.com>
2021-04-09 21:08:18 +0200

    Revert "first commit"
    This reverts commit 5f08d88cd77e2f3c1c925b363a2f3e8417215680.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import {BrowserRouter, Switch, Route, Router} from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import Home from './views/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      	<BrowserRouter>
-		      <Switch>
-            <Route path='/' component={Home}/>
-		      </Switch>
-        </BrowserRouter>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
