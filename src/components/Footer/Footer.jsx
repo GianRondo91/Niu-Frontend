3ebbde1d10c6f039754d1efdf74cea6365ecc641
Giancarlo <giancarlorondo@outlook.com>
2021-04-09 21:08:32 +0200

    Revert "Revert "first commit""
    This reverts commit c0d3c964a6d32f7bcbdbbb7b50cd992165bedf41.

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <div>
+            Soy el footer
+        </div>
+    )
+};
+
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <div>
+            Soy el header
+        </div>
+    )
+};
+
+export default Header;
