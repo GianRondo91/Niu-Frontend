98062a8f500d04d3d10b07cc3e3e661550dc0463
Giancarlo <giancarlorondo@outlook.com>
2021-05-06 20:59:22 +0200

    shop estructure

--- src/views/Shop/Shop.jsx
+++ src/views/Shop/Shop.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
 
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+
 const Shop = () => {
     return (
-        <div className="order-history">
-            <div className="history-title">TIENDA</div>
+        <div id="shop">
+            <Header />
+            <div id="shop-content">
+                TIENDA
+                <div className="product">
+                    <div className="product-img"></div>
+                    <div className="product-name">Crema de Calabaza</div>
+                    <div className="product-price">8€</div>
+                    <div className="product-button">Agregar</div>
+                </div>
+                <div className="product">
+                    <div className="product-img"></div>
+                    <div className="product-name">Crema de Calabaza</div>
+                    <div className="product-price">8€</div>
+                    <div className="product-button">Agregar</div>
+                </div>
+                <div className="product">
+                    <div className="product-img"></div>
+                    <div className="product-name">Crema de Calabaza</div>
+                    <div className="product-price">8€</div>
+                    <div className="product-button">Agregar</div>
+                </div>
+                <div className="product">
+                    <div className="product-img"></div>
+                    <div className="product-name">Crema de Calabaza</div>
+                    <div className="product-price">8€</div>
+                    <div className="product-button">Agregar</div>
+                </div>
+                <div className="product">
+                    <div className="product-img"></div>
+                    <div className="product-name">Crema de Calabaza</div>
+                    <div className="product-price">8€</div>
+                    <div className="product-button">Agregar</div>
+                </div><div className="product">
+                    <div className="product-img"></div>
+                    <div className="product-name">Crema de Calabaza</div>
+                    <div className="product-price">8€</div>
+                    <div className="product-button">Agregar</div>
+                </div>
+            </div>
+            <Footer />
         </div>
     )
 };
