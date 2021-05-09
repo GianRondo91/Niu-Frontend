102a1de91d0e159f623851ba15c83d4314f4fc6f
Giancarlo <giancarlorondo@outlook.com>
2021-05-09 14:46:50 +0200

    null check

--- src/views/UserContent/Shop/Starters/Starter.jsx
+++ src/views/UserContent/Shop/Starters/Starter.jsx
@@ -9,6 +9,11 @@ const Starter = (props) => {
     };
 
     const getProductElements = () => {
+
+        if(!props.products){
+            return null;
+        }
+
         let elements = [];
 
         for (let product of props.products){
